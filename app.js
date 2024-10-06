7627cfae7b4c73c3bc9655f5c3578d5947ac8fd3
Fenil Chodvadiya <151758131+Fenil412@users.noreply.github.com>
2024-10-06 19:00:58 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,80 @@
+const BASE_URL =
+  "https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies";
+
+const dropdowns = document.querySelectorAll(".dropdown select");
+const btn = document.querySelector("form button");
+const fromCurr = document.querySelector(".from select");
+const toCurr = document.querySelector(".to select");
+const msg = document.querySelector(".msg");
+const swap = document.querySelector("#swap");
+
+for (let select of dropdowns) {
+  for (currCode in countryList) {
+    let newOption = document.createElement("option")
+    newOption.innerText = currCode;
+    newOption.value = currCode;
+    if (select.name === "from" && currCode === "USD") {
+      newOption.selected = "selected";
+    } else if (select.name === "to" && currCode === "INR") {
+      newOption.selected = "selected";
+    }
+    select.append(newOption);
+  }
+
+  select.addEventListener("change", (evt) => {
+    updateFlag(evt.target);
+  });
+}
+
+const updateExchangeRate = async () => {
+  let amount = document.querySelector(".amount input");
+  let amtVal = amount.value;
+  if (amtVal === "" || amtVal < 1) {
+    amtVal = 1;
+    amount.value = "1";
+  }
+  const URL = `${BASE_URL}/${fromCurr.value.toLowerCase()}.json`;
+  let response = await fetch(URL);
+  let data = await response.json();
+  let rate = data[fromCurr.value.toLowerCase()][toCurr.value.toLowerCase()];
+
+  let finalAmount = amtVal * rate;
+  msg.innerText = `${amtVal} ${fromCurr.value} = ${finalAmount} ${toCurr.value}`;
+};
+
+const updateFlag = (element) => {
+  let currCode = element.value;
+  let countryCode = countryList[currCode];
+  let newSrc = `https://flagsapi.com/${countryCode}/flat/64.png`;
+  let img = element.parentElement.querySelector("img");
+  img.src = newSrc;
+};
+
+btn.addEventListener("click", (evt) => {
+  evt.preventDefault();
+  updateExchangeRate();
+});
+
+swap.addEventListener("click", () => {
+    let tempCurrency = fromCurr.value;
+  
+  // Swap the values
+  fromCurr.value = toCurr.value;
+  toCurr.value = tempCurrency;
+
+  // Update the flags after swapping
+  updateFlag(fromCurr);
+  updateFlag(toCurr);
+
+  // Update the exchange rate after swapping
+  updateExchangeRate();
+});
+
+
+window.addEventListener("load", () => {
+  let amount = document.querySelector(".amount input");
+
+  // Automatically set the default amount to 1 on page load
+  amount.value = "1";
+  updateExchangeRate();
+});
